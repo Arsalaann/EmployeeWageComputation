c90fa8333b7e9d9eb8650c895489460e6fb9c09e
Arsalaann <arsalankhanxk@gmail.com>
2023-04-26 08:04:45 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeeWage{
+    class Program{
+        public static void Main(string[] args){
+            Console.WriteLine("Welcome to employee wage computation program");
+        }
+    }
+
+}
