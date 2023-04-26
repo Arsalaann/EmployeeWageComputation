bf69c73e2929cad062686439dad2dcb052f07a56
Arsalaann <arsalankhanxk@gmail.com>
2023-04-26 09:06:01 +0530

    Added Employee wage

--- CheckEmployee.cs
+++ CheckEmployee.cs
@@ -1,13 +1,18 @@
 namespace EmployeeWage{
     class CheckEmployee{
         public static void CheckAttendance(){
+            int EMP_RATE_PER_HOUR=20;
             int IS_FULL_TIME=1;
+            int empHour=0;
+            int empWage=0;
             Random rnd=new Random();
             int empCheck=rnd.Next(2);
             if(empCheck==IS_FULL_TIME)
-                Console.WriteLine("Employee is present");
+                empHour=8;
             else
-                Console.WriteLine("Employee is not present");
+                empHour=0;
+            int Employee=EMP_RATE_PER_HOUR*empHour;
+            Console.WriteLine("Employee : "+Employee);
         }
     } 
 }
