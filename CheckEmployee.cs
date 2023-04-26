c01f7abf457a384d4d71f0d49ceab1c975e313b6
Arsalaann <arsalankhanxk@gmail.com>
2023-04-26 09:13:45 +0530

    Added part time code

--- CheckEmployee.cs
+++ CheckEmployee.cs
@@ -3,16 +3,19 @@ namespace EmployeeWage{
         public static void CheckAttendance(){
             int EMP_RATE_PER_HOUR=20;
             int IS_FULL_TIME=1;
+            int IS_PART_TIME=2;
             int empHour=0;
             int empWage=0;
             Random rnd=new Random();
-            int empCheck=rnd.Next(2);
+            int empCheck=rnd.Next(3);
             if(empCheck==IS_FULL_TIME)
                 empHour=8;
+            else if(empCheck==IS_PART_TIME)
+                empHour=4;
             else
                 empHour=0;
             empWage=EMP_RATE_PER_HOUR*empHour;
-            Console.WriteLine("Employee : "+empWage);
+            Console.WriteLine("Employee Wage: "+empWage);
         }
     } 
 }
